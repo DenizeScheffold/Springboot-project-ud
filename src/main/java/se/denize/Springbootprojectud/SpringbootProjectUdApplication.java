7af371f4d6503c9d57561e71759ab874ecb00921
Denize <DenizeScheffold@gmail.com>
2022-05-04 19:21:19 +0200

    Initial commit

---
+++ src/main/java/se/denize/Springbootprojectud/SpringbootProjectUdApplication.java
@@ -0,0 +1,13 @@
+package se.denize.Springbootprojectud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectUdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectUdApplication.class, args);
+	}
+
+}
